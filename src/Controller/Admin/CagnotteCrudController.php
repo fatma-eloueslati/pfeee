417f8aa7944b40c7ac930a8551fe43dc76c9c372
kawther20 <kawther.messaoudi@isitc.u-sousse.tn>
2021-06-10 13:37:09 +0100

    main

--- src/Controller/Admin/CagnotteCrudController.php
+++ src/Controller/Admin/CagnotteCrudController.php
@@ -4,6 +4,16 @@ namespace App\Controller\Admin;
 
 use App\Entity\Cagnotte;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\field\dateField;
+use EasyCorp\Bundle\EasyAdminBundle\field\AssociationField;
+
+
+
 
 class CagnotteCrudController extends AbstractCrudController
 {
@@ -12,14 +22,19 @@ class CagnotteCrudController extends AbstractCrudController
         return Cagnotte::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('Titre'),
+            TextEditorField::new('Description'),
+            ImageField::new('urlphoto'),
+            TextField::new('Beneficaire'),
+            MoneyField::new('Objectif'),
+            DateField::new('Deadline'),
+
+
         ];
     }
-    */
 }
